19b39e890d732596b2cd811472ff777743081d10
msobriquet <danir8228@gmail.com>
2023-11-12 03:01:34 -0500

    replaced sprites, added collisions and score

--- main.py
+++ main.py
@@ -3,32 +3,43 @@ import pygame
 class Dot(pygame.sprite.Sprite):
     def __init__(self, width, height, x, y):
         super().__init__()
-        self.image = pygame.image.load('graphics/note.png').convert_alpha()
+        self.image = pygame.image.load('graphics/dot.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height)) #whatever size in here is the size of the rect
         self.rect = self.image.get_rect() #just draws a rect directly around it
         self.rect.center = [x, y]
 
     def update(self):
-        self.rect.move_ip(-5, 0)
+        self.rect.move_ip(-8, 0)
         
 class Girl(pygame.sprite.Sprite):
     def __init__(self, width, height, x, y) -> None: #if i need width and height attributes I can add them
         super().__init__()
-        self.image = pygame.image.load('graphics/mc.png').convert_alpha()
+        self.image = pygame.image.load('graphics/girl.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height)) 
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.vel = 8
+        self.isLeft = False
+        self.isRight = False
         self.isJump = False
-        self.jumpCount = 10
+        self.jumpCount = 20
 
     def update(self): 
+        if self.isLeft == True:
+            self.rect.centerx -= self.vel
+            self.isLeft = False
+
+        if self.isRight == True:
+            self.rect.centerx += self.vel
+            self.isRight = False
+
         if self.isJump == True:
-            if self.jumpCount >= -10:
-                self.rect.move_ip(0, -(self.jumpCount * abs(self.jumpCount)) * 0.5) #auto updates rect object
+            if self.jumpCount >= -20:
+                self.rect.move_ip(0, -(self.jumpCount * abs(self.jumpCount)) * 0.3) #auto updates rect object
                 self.jumpCount -= 1
             else:
                 self.rect.move_ip(0, 0)
-                self.jumpCount = 10
+                self.jumpCount = 20
                 self.isJump = False
 
 class Background:
@@ -56,10 +67,12 @@ class Game:
     def __init__(self) -> None:
         self.background = [Background(0), Background(WIDTH)]
         self.speed = 3
-        self.girl = Girl(300, 300, 275, 550)
+        self.girl = Girl(150, 380, 275, 550)
         self.girl_group = pygame.sprite.Group()
         self.dot_group = pygame.sprite.Group()
         self.girl_group.add(self.girl)
+        self.damage_score = 0 # num of collisions with dots
+        self.font = pygame.font.SysFont('Comic Sans MS', 30)
 
 #general setup
 pygame.init()
@@ -71,13 +84,14 @@ HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #song data
-data = [5.000, 6.000, 7.000, 8.000] #where the beats of the song hit in seconds
+data = [5.000, 7.000, 9.000, 11.000] #where the beats of the song hit in seconds
 
 #game loop
 def main():
     running = True
     thisGame = Game()
     l_time = 0
+
 
     while running:
         for event in pygame.event.get():
@@ -89,6 +103,17 @@ def main():
             bg.update(-thisGame.speed) #paralax effect
             bg.show()  #constructor only shows it once so we need to show it every time
 
+        #score
+        text_surface = thisGame.font.render("Score:" + str(len(data) - thisGame.damage_score)+ " out of " + str(len(data)), False, (0, 0, 0))    
+
+        # See if the Sprite block has collided with anything in the Group dot_group
+        # The True flag will remove the sprite in block_list
+        dot_group_hits = pygame.sprite.spritecollide(thisGame.girl, thisGame.dot_group, True)
+
+        # Check the list of colliding sprites, and add one to the score for each one
+        for hit in dot_group_hits:
+            thisGame.damage_score +=1    
+
         #player
         keys = pygame.key.get_pressed()
 
@@ -98,7 +123,16 @@ def main():
         else:
             thisGame.girl_group.update()
 
+        if keys[pygame.K_LEFT] and thisGame.girl.rect.centerx > thisGame.girl.vel:  # Making sure the top left position of our character is greater than our vel so we never move off the screen.
+            thisGame.girl.isLeft = True
+            thisGame.girl_group.update()
 
+        if keys[pygame.K_RIGHT] and thisGame.girl.rect.centerx <  WIDTH:  # Making sure the top right corner of our character is less than the screen width
+            thisGame.girl.isRight = True
+            thisGame.girl_group.update()   
+
+
+        #dots
         thisGame.dot_group.update()
 
         print("current time:")
@@ -111,13 +145,16 @@ def main():
 
         for i in range(len(data)):
             if c_time >= data[i] and l_time < data[i]:
-                dot = Dot(100, 100, 1280, 400)
+                dot = Dot(80, 80, 1280, 600)
                 thisGame.dot_group.add(dot)
 
         l_time = c_time
 
+        #rendering
         thisGame.dot_group.draw(screen)
         thisGame.girl_group.draw(screen)
+
+        screen.blit(text_surface, (0,0))
 
         pygame.display.update()
         clock.tick(60)
